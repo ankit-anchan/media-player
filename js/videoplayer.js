79d743cf8b9ae54685129f876045961e702f9d23
Ankit Anchan <ankit.anchan9767@gmail.com>
2019-11-15 15:05:06 +0530

    ankit-1 -- integrated with shaka player with dash streaming

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -1,238 +1,43 @@
-var videoTag = document.getElementById("videoplayer");
-var loadingBar = document.getElementById("loadingBar");
-// videoTag.src="videos/lecture.mp4";
-// videoTag.src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-videoTag.src = "https://www.radiantmediaplayer.com/media/bbb-360p.mp4";
-isVideoPlaying = false;
-isVideoMuted = false;
-progressPercent = 0;
-isFullScreen = false;
-hideControlsTimeout = null;
-document.getElementById("showProgress").innerText = "00:00";
+var manifestUri =
+    'videos/dash.mpd';
 
-bindVideoEvents();
-bindVideoContainerEvents();
-bindVideoControlEvents();
 
-function toggleMute() {
-	if (isVideoMuted === true) {
-		showIconOnCenter("images/volume-up-indicator.png");
-		isVideoMuted = false;
-		changeVolume(1)
-		document.getElementById("muteButton").src = "images/volume-up-indicator.png";
-	} else {
-		showIconOnCenter("images/volume-off-indicator.png");
-		isVideoMuted = true;
-		changeVolume(0)
-		document.getElementById("muteButton").src = "images/volume-off-indicator.png";
-	}
+function initApp() {
+  shaka.polyfill.installAll();
+
+  // Check to see if the browser supports the basic APIs Shaka needs.
+  if (shaka.Player.isBrowserSupported()) {
+    // Everything looks good!
+    initPlayer();
+  } else {
+    // This browser does not have the minimum set of APIs we need.
+    console.error('Browser not supported!');
+  }
 }
 
-function playVideo () {
-	showControls();
-	isVideoPlaying = true;
-	videoTag.play();
-	document.getElementById("playButton").src = "images/pause-symbol.png";
-	hideControls();
+function initPlayer() {
+	var video = document.getElementById("videoplayer");
+  	var player = new shaka.Player(video);
+
+  	window.player = player;
+
+  	// Listen for errors
+  	player.addEventListener('error', onErrorEvent);
+	player.load(manifestUri).then(function() {
+	    // This runs if the asynchronous load is successful.
+	    console.log('The video has now been loaded!');
+	})
+	.catch(onError);
 }
 
-function showIconOnCenter(resource) {
-	loadingBar.style.display = "block";
-	loadingBar.src = resource;
-	setTimeout(() => {
-		loadingBar.style.display = "none";
-	}, 500);
+function onErrorEvent(event) {
+  // Extract the shaka.util.Error object from the event.
+  onError(event.detail);
 }
 
-function showControls() {
-	document.getElementById("controlsTab").style.display = "block";
+function onError(error) {
+  // Log the error.
+  console.error('Error code', error.code, 'object', error);
 }
 
-function hideControls() {
-	clearTimeout(hideControlsTimeout);
-	hideControlsTimeout = setTimeout(() => {	
-		document.getElementById("controlsTab").style.display = "none";
-	}, 2000);
-}
-
-function pauseVideo () {
-	showControls();
-	isVideoPlaying = false;
-	videoTag.pause();
-	document.getElementById("playButton").src = "images/play.png"
-	hideControls();
-}
-
-function forwardTenSeconds () {
-	videoTag.currentTime += 10;
-}
-
-function backTenSeconds () {
-	videoTag.currentTime -= 10;
-}
-
-function onDurationChange(event) {
-	var duration = getFormattedTime(videoTag.duration)
-	document.getElementById("showDuration").innerText = duration.minutes + ":" + duration.seconds;
-}
-
-function getFormattedTime(totalTime) {
-	let minutes = totalTime / 60;
-	let seconds = totalTime % 60;
-	return {minutes: ("0" + parseInt(minutes)).slice(-2), seconds: ("0" + parseInt(seconds)).slice(-2) }
-}
-
-function onTimeUpdate (event) {
-	var mins = videoTag.currentTime / 60;
-	var seconds = videoTag.currentTime % 60;
-	progressPercent = parseInt((videoTag.currentTime * 100) / videoTag.duration);
-	document.getElementById("showProgress").innerText = ("0" + parseInt(mins)).slice(-2) + ":" + ("0" + parseInt(seconds)).slice(-2);
-	document.getElementById("myProgressBar").style.width = `${progressPercent}%`;
-}
-
-function onSeeking () {
-	loadingBar.src = "images/loading.gif";
-	loadingBar.style.display = "block";
-	if (isVideoPlaying) {
-		togglePlayPause();
-	}
-}
-
-function onSeeked () {
-	loadingBar.src = "images/loading.gif";
-	loadingBar.style.display = "none";
-	if (!isVideoPlaying) {
-		togglePlayPause();
-	}
-}
-
-function showFullScreen() {
-	var videoContainer = document.getElementById("contain");
-	document.getElementById("fullScreenButton").style.display = "none";
-	document.getElementById("exitFullScreenButton").style.display = "block";
-	if (videoContainer.requestFullscreen) {
-		videoContainer.requestFullscreen();
-	} else if (videoContainer.mozRequestFullScreen) {
-		videoContainer.mozRequestFullScreen();
-	} else if (videoContainer.webkitRequestFullscreen) {
-		videoContainer.webkitRequestFullscreen();
-	} else if (videoContainer.msRequestFullscreen) { 
-		videoContainer.msRequestFullscreen();
-	}
-}
-
-function exitFullScreen() {
-	if (document.exitFullscreen) {
-	    document.exitFullscreen();
-	  } else if (document.mozCancelFullScreen) { /* Firefox */
-	    document.mozCancelFullScreen();
-	  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-	    document.webkitExitFullscreen();
-	  } else if (document.msExitFullscreen) { /* IE/Edge */
-	    document.msExitFullscreen();
-	  }
- 	document.getElementById("fullScreenButton").style.display = "block";
-	document.getElementById("exitFullScreenButton").style.display = "none";
-}
-
-function handleKeyPress(event) {
-	keyPressActions(event);
-}
-
-function keyPressActions (pressedKeyEvent) {
-	switch (pressedKeyEvent.charCode) {
-		case 32: 
-			playPauseControls();
-			return;
-	}
-	switch (pressedKeyEvent.keyCode) {
-		case 39:
-			forwardTenSeconds();
-			break;
-		case 37:
-			backTenSeconds();
-			break;
-	}
-}
-
-function onMouseEnter() {
-	clearTimeout(hideControlsTimeout);
-	showControls();
-}
-
-function onMouseMove() {
-	showControls();
-	hideControls();
-}
-
-function handleLoadedBufferedData() {
-	let buffered = videoTag.buffered.end(0);	
-	const videoDuration = videoTag.duration;
-	var percent = parseInt((buffered / videoDuration) * 100);
-	console.log(`percent loaded ${percent}`);
-	document.getElementById("myBar").style.width = `${percent}%`;
-}
-
-function toggleFullScreen() {
-	if (isFullScreen === true) {
-		isFullScreen = false;
-		exitFullScreen();
-	} else {
-		isFullScreen = true;
-		showFullScreen();
-	}
-}
-
-function togglePlayPause () {
-	if (isVideoPlaying) {
-		pauseVideo();
-	} else {
-		playVideo();
-	}
-}
-
-function changeVolume (volume) {
-	videoTag.volume = volume;
-}
-
-function playPauseControls() {
-	if (isVideoPlaying) {
-		showIconOnCenter("images/pause-symbol.png");
-		pauseVideo();
-	} else {
-		showIconOnCenter("images/play.png");
-		playVideo();
-	}
-}
-
-function bindVideoContainerEvents() {
-	var videoPlayerContainer = document.getElementById("contain");
-	var videoContainer = document.getElementById("videoContainer")
-			.addEventListener("dblclick", toggleFullScreen);
-	videoPlayerContainer.addEventListener("mousemove", onMouseMove);
-	videoPlayerContainer.addEventListener("mouseenter", showControls);
-	videoPlayerContainer.addEventListener("mouseleave", hideControls);
-	videoPlayerContainer.addEventListener("keypress", handleKeyPress);
-	videoPlayerContainer.addEventListener("keydown", handleKeyPress);	
-}
-
-function bindVideoEvents() {
-	videoTag.addEventListener("timeupdate", onTimeUpdate);
-	videoTag.addEventListener("seeking", onSeeking);
-	videoTag.addEventListener("seeked", onSeeked);
-	videoTag.addEventListener("durationchange", onDurationChange);
-	videoTag.addEventListener("loadeddata", () => {
-		videoTag.addEventListener("progress", handleLoadedBufferedData);	
-	});
-}
-
-function bindVideoControlEvents() {
-	document.getElementById("fullScreenButton").addEventListener("click", showFullScreen);
-	document.getElementById("exitFullScreenButton").addEventListener("click", exitFullScreen);
-	document.getElementById("playButton").addEventListener("click", playPauseControls);
-	document.getElementById("backTenSecondsButton").addEventListener("click", backTenSeconds);
-	document.getElementById("forwardTenSecondsButton").addEventListener("click", forwardTenSeconds);
-	document.getElementById("muteButton").addEventListener("click", toggleMute);
-}
-
-
+document.addEventListener("DOMContentLoaded", initApp);
